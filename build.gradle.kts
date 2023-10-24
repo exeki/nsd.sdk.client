3a0d934b494ab7835a9f3653b6354d7d82769dfc
ekazantsev <ekazantsev@ocs.ru>
2023-10-24 18:05:53 +0400

    build.gradle переделан на kts

---
+++ build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+    kotlin("jvm") version "1.9.10"
+    id("maven-publish")
+    id("groovy")
+    id("org.jetbrains.dokka") version "1.9.10"
+}
+
+group = "ru.ekazantsev"
+version = "1.0.0"
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "11"
+    }
+
+    javadoc {
+        dependsOn(dokkaJavadoc)
+    }
+
+    dokkaJavadoc {
+        outputDirectory.set(buildDir.resolve("docs\\javadoc"))
+    }
+
+    register<Jar>("javadocJar") {
+        from(getByName("javadoc").outputs.files)
+        archiveClassifier.set("javadoc")
+    }
+
+    register<Jar>("sourcesJar") {
+        from(sourceSets.main.get().allSource)
+        archiveClassifier.set("sources")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["kotlin"])
+
+            artifact(tasks.named("javadocJar"))
+            artifact(tasks.named("sourcesJar"))
+
+            pom {
+                groupId = project.group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+            }
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+dependencies {
+    implementation("ru.ekazantsev:nsd_sdk_data:1.0.0")
+    implementation("ru.ekazantsev:nsd_basic_api_connector:1.0.0")
+    implementation("org.apache.httpcomponents:httpclient:4.5.14")
+    implementation("org.apache.httpcomponents:httpmime:4.5.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    implementation("org.slf4j:slf4j-api:2.0.9")
+
+    testImplementation("ch.qos.logback:logback-classic:1.4.11")
+    testImplementation("org.codehaus.groovy:groovy-all:3.0.19")
+    testImplementation(kotlin("test"))
+}
